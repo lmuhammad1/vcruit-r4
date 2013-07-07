07c8e1d2d9e1e6a807b819b2dde65cebab6fa8bc
Louis Muhammad <louis.muhammad@gmail.com>
2013-07-07 08:44:50 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vcruit::Application.config.secret_key_base = 'ef83367a84782c8eaf040b50cf0eb8abe345463e243fb519ef1a15dedfaba7b38824ec1de51bf3d45c1c8f315c5ee0bace5f6d054858befafb83d9c17187a56e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vcruit::Application.config.session_store :cookie_store, key: '_vcruit_session'
