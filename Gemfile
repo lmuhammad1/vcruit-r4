8aff6d63335bd1ba48140a0dbdfeaeb005261832
Louis Muhammad <louis.muhammad@gmail.com>
2013-07-07 20:50:22 -0400

    added user authentication with devise 3.0.0.rc gem

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+gem 'devise', '3.0.0.rc'
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Vcruit::Application.routes.draw do
 
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  
 	root :to => 'static_pages#home'	
 
 	get 'about', to: 'static_pages#about', as: 'about'
