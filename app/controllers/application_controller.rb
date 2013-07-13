7981e74ff5f7f945cf83023ea72d5357de63d12e
Louis Muhammad <louis.muhammad@gmail.com>
2013-07-13 04:15:33 -0400

    Created login helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,10 @@ class ApplicationController < ActionController::Base
     @current_user_profile = Profile.find_by user_id: current_user
   end
   
+  def login_name
+    login = "#{current_user_profile.first_name.capitalize} #{current_user_profile.last_name.capitalize}"
+  end
+  
   helper_method :current_user_profile
+  helper_method :login_name
 end
