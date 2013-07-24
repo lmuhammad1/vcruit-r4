032822f81636ba57d757473f9eb9f78d8a6dd0aa
Louis Muhammad <louis.muhammad@gmail.com>
2013-07-24 15:34:21 -0400

    adjust video controller and change location of paperclip storage

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,8 @@ class Profile < ActiveRecord::Base
   validates_uniqueness_of :user_id
   belongs_to :user
   has_many :video_pages
-  has_attached_file :logo, :styles => { :small => "150x150>"}
+  has_attached_file :logo, :styles => { :small => "150x150>"},
+    :url => "/assets/company_logos/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/assets/company_logos/:id/:style/:basename.:extension"
+  
 end
