c7c51096f4dd2df3ddec756163f874969c6fea5e
Louis Muhammad <louis.muhammad@gmail.com>
2013-08-12 12:29:04 -0400

    remove fog gem and added aws gem and config paperclip to upload to aws

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,8 +2,7 @@ class Profile < ActiveRecord::Base
   validates_uniqueness_of :user_id
   belongs_to :user
   has_many :video_pages
-  has_attached_file :logo, :styles => { :small => "150x150>"},
-    :url => "/assets/company_logos/:id/:style/:basename.:extension",
-    :path => ":rails_root/public/assets/company_logos/:id/:style/:basename.:extension"
   
+  has_attached_file :logo, :styles => { :small => "150x150>"}
+    
 end
