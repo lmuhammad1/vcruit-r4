42a83c1785a99be45ee01f5c20406c18d93869bf
Louis Muhammad <louis.muhammad@gmail.com>
2013-08-20 05:11:21 -0400

    add url_check method in profile.rb to add http:// to social medial links if not present

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,33 @@
 class Profile < ActiveRecord::Base
   validates_uniqueness_of :user_id
+  before_validation :url_check
   belongs_to :user
   has_many :video_pages
   
   has_attached_file :logo, :styles => { :small => "150x150>"}
-    
+  
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :phone, :numericality => true, 
+                    :length => { :minimum => 10, :maximum => 15 }
+                    
+
+  
+  
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:   true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+                
+
+  protected
+  def url_check
+      if ! self.facebook.blank? && !(self.facebook[/^http:\/\//] || self.facebook[/^https:\/\//])
+        self.facebook = 'http://' + self.facebook
+      elsif ! self.twitter.blank? && !(self.twitter[/^http:\/\//] || self.twitter[/^https:\/\//])
+        self.twitter = 'http://' + self.twitter
+      elsif ! self.linkedin.blank? && !(self.linkedin[/^http:\/\//] || self.linkedin[/^https:\/\//])
+        self.linkedin = 'http://' + self.linkedin
+      end
+      
+    end
 end
