d76f8726ea12548e84b50530c4c20a91f0d9dbfd
Louis Muhammad <louis.muhammad@gmail.com>
2013-09-03 15:58:44 -0400

    Add watermark video play button on linked image

--- app/models/video_page.rb
+++ app/models/video_page.rb
@@ -1,7 +1,18 @@
+require 'paperclip_processors/watermark'
+
 class VideoPage < ActiveRecord::Base
   extend FriendlyId
   belongs_to :profile
   friendly_id :permalink
   
-  has_attached_file :thumbnail, :styles => { :small => "150x150>", :medium => "300x300>" }
+  has_attached_file :thumbnail, 
+                    :processors => [:watermark],
+                      :styles => {
+                        :medium => {
+                          :geometry       => "400x300>",
+                          :position       => "SouthEast",
+                          :watermark_path => "#{Rails.root}/public/images/watermark.png"
+                        },
+                        :small => "150x150>",
+                      }
 end
